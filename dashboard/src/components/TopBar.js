19ae9439ac1b6df2266e39073c77051824aaa03a
Rakesh7297 <rakeshdhakharwal0507@gmail.com>
2026-01-14 11:05:31 +0530

    adding my  name beside logo in dashboard full

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -1,15 +1,22 @@
 import React from "react";
-
 import Menu from "./Menu";
 
 const TopBar = () => {
   return (
-    <div className="topbar-container">
+    <div
+      className="topbar-container"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      {/* Left: Indices */}
       <div className="indices-container">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
-          <p className="index-points">{100.2} </p>
-          <p className="percent"> </p>
+          <p className="index-points">{100.2}</p>
+          <p className="percent"></p>
         </div>
         <div className="sensex">
           <p className="index">SENSEX</p>
@@ -18,7 +25,26 @@ const TopBar = () => {
         </div>
       </div>
 
-      <Menu />
+      {/* Right: Name + Menu */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "20px",
+        }}
+      >
+        <span
+          style={{
+            fontWeight: "500",
+            color: "#444",
+            fontSize: "14px",
+          }}
+        >
+          by Rakesh Dhakharwal
+        </span>
+
+        <Menu />
+      </div>
     </div>
   );
 };
